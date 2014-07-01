21a02a6082041dbb03c071b17a3cbd61dfbaf50e
thewirl <dyngar-m@dyngar-m.ddns01.com>
2014-07-01 07:17:33 -0400

    checking to make sure gitignore ignores *~ files

---
+++ lb-clean.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+cd wulfric-iso && sudo lb clean
+cd ..
+cd jean-iso && sudo lb clean
--- lb-nightly.sh
+++ lb-nightly.sh
@@ -20,6 +20,7 @@ else
 fi
 # clean up the configuration
 sudo lb clean
+sudo lb init
 # pre-configure the base system
 sudo lb config ­-b iso --cache true --apt-recommends --distribution jessie true -a amd64  --linux-flavour=3.14-1-amd64 --binary-images iso ­­--debian-installer live --mode debian ­--debian­-installer-gui false ­­--archive-areas "main" ­­--security true --win32-loader false ­­--updates true 
 # copying base system sources.list
@@ -29,11 +30,11 @@ sudo cp ../live.list.chroot config/package-lists/$DISTRO.list.chroot
 # copying base system apt preferences
 sudo cp ../preferences.list.chroot config/archives/$DISTRO.pref.chroot
 # copying firewall configuration
-mkdir -p config/includes.chroot/etc/ferm/
+sudo mkdir -p config/includes.chroot/etc/ferm/
 sudo cp ../ferm.conf config/includes.chroot/etc/ferm/ferm.conf
 # creating /etc/skel directory
-# mkdir -p config/includes.chroot/etc/skel/
+# sudo mkdir -p config/includes.chroot/etc/skel/
 # importing Tor keys
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 > tor.key.chroot
 sudo mv tor.key.chroot config/archives/tor.key.chroot
-sudo lb build
+#sudo lb build
